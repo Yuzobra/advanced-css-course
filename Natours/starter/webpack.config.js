486e9609e1fddff0f012ea915fbb678549285110
Yuri Zoel Brasil <yuri.zoel@olxbr.com>
2019-10-25 18:18:17 -0300

    new sections added

---
+++ Natours/starter/webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    mode: "development",
+    entry: ["./sass/main.scss"],
+    module: {
+      rules: [
+        {
+          test: /\.(s*)css$/,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true, // webpack@1.x
+                disable: true, // webpack@2.x and newer
+              },
+            },
+          ],
+        }
+      ],
+    },
+  };
